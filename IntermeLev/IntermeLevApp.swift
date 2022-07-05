25c587d8338e9b3a93ab0356a544f1903681ebf7
SaberHardy <samirsamado5@gmail.com>
2022-07-05 17:12:55 +0300

    Initial Commit

---
+++ IntermeLev/IntermeLevApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermeLevApp.swift
+//  IntermeLev
+//
+//  Created by MacBook on 05.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct IntermeLevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
