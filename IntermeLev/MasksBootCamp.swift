a16db083450842da91c883f77f7309eb4db8cc72
SaberHardy <samirsamado5@gmail.com>
2022-07-07 14:50:03 +0300

    add masking to stars

--- IntermeLev/MasksBootCamp.swift
+++ IntermeLev/MasksBootCamp.swift
@@ -10,13 +10,34 @@ import SwiftUI
 struct MasksBootCamp: View {
     @State var rating: Int = 1
     var body: some View {
+        ZStack {
+            starsView
+                .overlay(overlayView.mask(starsView))
+        }
+    }
+    
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .foregroundColor(.yellow)
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View {
         HStack {
             ForEach(1 ..< 6) { index in
                 Image(systemName: "star.fill")
                     .font(.title)
                     .foregroundColor(rating >= index ? Color.yellow : Color.gray)
                     .onTapGesture {
-                        rating = index
+                        withAnimation(.spring()){
+                            rating = index
+                        }
+                        
                     }
             }
         }
