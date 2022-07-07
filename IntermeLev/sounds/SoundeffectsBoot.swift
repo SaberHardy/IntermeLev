83822bbbe0683b76d90c3180b3b02cfdcf40a97f
SaberHardy <samirsamado5@gmail.com>
2022-07-07 18:48:00 +0300

    vibrations(haptics)

--- IntermeLev/sounds/SoundeffectsBoot.swift
+++ IntermeLev/sounds/SoundeffectsBoot.swift
@@ -28,24 +28,52 @@ class SoundManager {
     
 }
 
+class HapticsManager {
+    static let instance = HapticsManager() // Singelton
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
+
 struct SoundeffectsBoot: View {
     var body: some View {
         VStack {
-            Button("Play Sound 1") {
-                SoundManager.instance.playSound(sound: .am)
-            }
-            .font(.largeTitle)
-            .padding()
-            .background(.green)
-            .cornerRadius(20)
+            //                Button("Play Sound 1") {
+            //                    SoundManager.instance.playSound(sound: .am)
+            //                }
+            //                .font(.largeTitle)
+            //                .padding()
+            //                .background(.green)
+            //                .cornerRadius(20)
+            //
+            //                Button("Play Sound 2") {
+            //                    SoundManager.instance.playSound(sound: .water)
+            //                }
+            //                .font(.largeTitle)
+            //                .padding()
+            //                .background(.gray)
+            //                .cornerRadius(20)
             
-            Button("Play Sound 2") {
-                SoundManager.instance.playSound(sound: .water)
-            }
-            .font(.largeTitle)
-            .padding()
-            .background(.gray)
-            .cornerRadius(20)
+            //                Spacer()
+            
+            Button("Success") { HapticsManager.instance.notification(type: .success)}
+            Button("Error") { HapticsManager.instance.notification(type: .error)}
+            Button("Warning") { HapticsManager.instance.notification(type: .warning)}
+            
+            Divider()
+            
+            Button("Soft") { HapticsManager.instance.impact(style: .soft) }
+            Button("heavy") { HapticsManager.instance.impact(style: .heavy) }
+            Button("light") { HapticsManager.instance.impact(style: .light) }
+            Button("medium") { HapticsManager.instance.impact(style: .medium) }
+            Button("rigid") { HapticsManager.instance.impact(style: .rigid) }
             
         }
     }
