5666782fc0a26f37860a55ca9b0e2c63c7dfa971
SaberHardy <samirsamado5@gmail.com>
2022-07-07 12:18:30 +0300

    use $item multi sheets

--- IntermeLev/MultipleSheetsBoot.swift
+++ IntermeLev/MultipleSheetsBoot.swift
@@ -19,48 +19,31 @@ struct NewModel: Identifiable {
  3 - use $item
  ****/
 struct MultipleSheetsBoot: View {
-    @State var selectedModel: NewModel = NewModel(title: "First Title")
-    @State var showSheet: Bool = false
-    @State var showSheet2: Bool = false
+    @State var selectedModel: NewModel? = nil
+    
     var body: some View {
-        VStack(spacing: 20) {
-            Button("Button 1") {
-//                selectedModel = NewModel(title: "One")
-                showSheet.toggle()
+        ScrollView {
+            VStack(spacing: 20) {
+                ForEach(0..<50) { index in
+                    Button("Button \(index)") {
+                        selectedModel = NewModel(title:"\(index)")
+                    }
+                    .frame(width: 150, height: 60)
+                    .font(.title)
+                    .background(.green)
+                    .foregroundColor(.blue)
+                    .cornerRadius(30)
+                }
             }
-            .sheet(isPresented: $showSheet, content: {
-                nextScreen(
-                    selectedModel: NewModel(title: "One sheet"))
-            })
-            .frame(width: 150, height: 60)
-            .font(.largeTitle)
-            .background(.green)
-            .foregroundColor(.blue)
-            .cornerRadius(30)
-            
-            Button("Button 2") {
-//                selectedModel = NewModel(title: "Two")
-                
-                showSheet2.toggle()
-            }
-            .sheet(isPresented: $showSheet2, content: {
-                nextScreen(
-                    selectedModel: NewModel(title: "Two sheet"))
-            })
-            .frame(width: 150, height: 60)
-            .font(.largeTitle)
-            .background(.green)
-            .foregroundColor(.blue)
-            .cornerRadius(30)
+            .sheet(item: $selectedModel) { model in
+                nextScreen(selectedModel: model)
         }
-//        .sheet(isPresented: $showSheet, content: {
-//            nextScreen(selectedModel: $selectedModel)
-//        })
+        }
     }
 }
 
 struct nextScreen: View {
-//    @Binding var selectedModel: NewModel
+    //    @Binding var selectedModel: NewModel
     let selectedModel: NewModel
     
     var body: some View {
