8dc7c565e6f504c278bdeb294c88c060426f4cce
SaberHardy <samirsamado5@gmail.com>
2022-07-08 15:40:03 +0300

    map the data with nil values

--- ArraysTechs.swift
+++ ArraysTechs.swift
@@ -5,7 +5,7 @@ import SwiftUI
  */
 struct UserModel: Identifiable {
     let id = UUID().uuidString
-    let name: String
+    let name: String?
     let points: Int
     let isVerified: Bool
 }
@@ -14,6 +14,7 @@ class ArrayModelView: ObservableObject {
     
     @Published var dataArray: [UserModel] = []
     @Published var filteredArray: [UserModel] = []
+    @Published var mappedArray: [String] = []
     
     init() {
         getUserModel()
@@ -32,14 +33,29 @@ class ArrayModelView: ObservableObject {
 //        filteredArray = dataArray.filter({ (user) -> Bool in
 //            return user.isVerified
 //        })
-        filteredArray = dataArray.filter({ $0.isVerified })
+//        filteredArray = dataArray.filter({ $0.isVerified })
+        
+        /* Map Data */
+//        mappedArray = dataArray.map({ (user) -> String in
+//            return user.name
+//        })
+        
+//        mappedArray = dataArray.map({ $0.name })
+        
+        
+//        What if we have some users doesn't have name? => nil
+        // the solution is to exclude these names from user interfce
+//        mappedArray = dataArray.compactMap({ (user) -> String? in
+//            return user.name
+//        })
+         mappedArray = dataArray.compactMap({ $0.name })
         
     }
     
     func getUserModel() {
         let user1 = UserModel(name: "Saber", points: 3, isVerified: true)
         let user2 = UserModel(name: "Khiro", points: 4, isVerified: true)
-        let user3 = UserModel(name: "Mahdi", points: 2, isVerified: false)
+        let user3 = UserModel(name: nil, points: 2, isVerified: false)
         let user4 = UserModel(name: "Khali", points: 5, isVerified: true)
         let user5 = UserModel(name: "Mama", points: 5, isVerified: false)
         
@@ -53,22 +69,27 @@ struct ArraysTechs: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading ,spacing:20) {
-                ForEach(vm.filteredArray) { item in
-                    HStack {
-                        Text(item.name)
-                            .foregroundColor(.white)
-                            
-                        Text("\(item.points)")
-                        Spacer()
-                        Image(systemName: item.isVerified ? "flame.fill" : "flame")
-                            .foregroundColor(item.isVerified ? Color.red : Color.gray)
-                    }
-                    .padding()
-                    .font(.title)
-                    .background(Color.blue.cornerRadius(20))
-                    .frame(maxWidth: .infinity)
-                    .padding(.horizontal)
+                ForEach(vm.mappedArray, id: \.self) { user in
+                    Text(user)
+                        .font(.title)
                 }
+                
+//                ForEach(vm.filteredArray) { item in
+//                    HStack {
+//                        Text(item.name)
+//                            .foregroundColor(.white)
+//
+//                        Text("\(item.points)")
+//                        Spacer()
+//                        Image(systemName: item.isVerified ? "flame.fill" : "flame")
+//                            .foregroundColor(item.isVerified ? Color.red : Color.gray)
+//                    }
+//                    .padding()
+//                    .font(.title)
+//                    .background(Color.blue.cornerRadius(20))
+//                    .frame(maxWidth: .infinity)
+//                    .padding(.horizontal)
+//                }
             }
         }
     }
